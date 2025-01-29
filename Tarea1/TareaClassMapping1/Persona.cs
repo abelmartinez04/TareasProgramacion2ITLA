26ec2b90b87f57a47220340417c83ae93cc3f482
Abel Martinez <abelrobles0409@gmail.com>
2025-01-28 21:43:39 -0400

    Añadiendo la primera clase

---
+++ Tarea1/TareaClassMapping1/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaClassMapping1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //empezar aqui
+            Persona persona1 = new Persona();
+            persona1.Name = "Juan";
+            Console.Write(persona1.Name);
+        }
+    }
+    
+    public class Persona
+    {
+        public string Name { get; set; }
+    }
+}
