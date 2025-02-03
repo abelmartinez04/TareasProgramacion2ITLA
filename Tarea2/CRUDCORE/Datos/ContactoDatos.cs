49170d64db8fcfb96c05e5eb1ef92906786d76c0
Abel Martinez <abelrobles0409@gmail.com>
2025-02-03 11:25:57 -0400

    Desarrollando codigo para usar con la conexion de SQLSERVer
    *Tuve que instalar otro paquete de SQL client porque el otro esta obsoleto*

---
+++ Tarea2/CRUDCORE/Datos/ContactoDatos.cs
@@ -0,0 +1,104 @@
+﻿using CRUDCORE.Models;
+using System.Data.SqlClient;
+using System.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CRUDCORE.Datos
+{
+    public class ContactoDatos
+    {
+        [Obsolete]
+        public List<ContactoModel> Listar()
+        {
+            var oLista = new List<ContactoModel>();
+
+            var cn = new Conexion();
+
+            using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_Listar", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+
+                    while (dr.Read())
+                    {
+                        oLista.Add(new ContactoModel
+                        {
+                            IdContact = Convert.ToInt32(dr["IdContact"]),
+                            Nombre = dr["Nombre"].ToString(),
+                            Telefono = dr["Telefono"].ToString(),
+                            Correo = dr["Correo"].ToString()
+                        });
+
+                    }
+                }
+            }
+            return oLista;
+        }
+
+
+        public ContactoModel Obtener(int IdContact)
+        {
+            var oContacto = new ContactoModel();
+
+            var cn = new Conexion();
+
+            using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_Obtener", conexion);
+                cmd.Parameters.AddWithValue("IdContact", IdContact);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+
+                    while (dr.Read())
+                    {
+                        oContacto.IdContact = Convert.ToInt32(dr["IdContact"]);
+                        oContacto.Nombre = dr["Nombre"].ToString();
+                        oContacto.Telefono = dr["Telefono"].ToString();
+                        oContacto.Correo = dr["Correo"].ToString();
+
+                    }
+                }
+            }
+            return oContacto;
+        }
+    
+        public bool Guardar(ContactoModel ocontacto)
+        {
+            bool rpta;
+
+            try
+            {
+                var cn = new Conexion();
+
+                using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Guardar", conexion);
+                    cmd.Parameters.AddWithValue("Nombre", ocontacto.Nombre);
+                    cmd.Parameters.AddWithValue("Telefono", ocontacto.Telefono);
+                    cmd.Parameters.AddWithValue("Correo", ocontacto.Correo);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception e)
+            {
+                string errro = e.Message;
+                rpta=false;
+            }
+
+
+            return rpta;
+            //me quede aqui: https://youtu.be/qcRCDM5KiSo?si=Nv8yuAN3M6cfiSXW&t=2321
+        }
+
+    }
+}
