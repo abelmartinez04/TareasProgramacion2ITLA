510b2165f79c0b3ad932829ca1938d4f00668be5
Abel Martinez <abelrobles0409@gmail.com>
2025-02-03 10:01:32 -0400

    Creacion del Query en SQLserver
    Se creo el query que va a estar utilizando el query de nuestro proyecto

---
+++ Tarea2/Crudquery.sql
@@ -0,0 +1,58 @@
+
+create database DBCRUDCORE
+
+USE DBCRUDCORE
+
+CREATE TABLE CONTACTO(
+IdContact int identity primary key,
+Nombre varchar(50),
+Telefono varchar(50),
+Correo varchar(50)
+)
+
+create procedure sp_Listar
+as
+begin
+	select * from CONTACTO
+end
+
+create procedure sp_Obtener(
+@IdContact int
+)
+as
+begin
+	select * from CONTACTO where IdContact = @IdContact
+end
+
+
+
+create procedure sp_Guardar(
+@Nombre varchar(100),
+@Telefono varchar(100),
+@Correo varchar(100)
+)
+as
+begin
+	insert into CONTACTO(Nombre,Telefono,Correo) values (@Nombre,@Telefono,@Correo)
+	end
+
+
+create procedure sp_Editar(
+@Id_contact int,
+@Nombre varchar(100),
+@Telefono varchar(100),
+@Correo varchar(100)
+)
+as
+begin
+	update CONTACTO set Nombre = @Nombre, Telefono = @Telefono, Correo = @Correo where IdContact = IdContact
+end
+
+
+create procedure sp_Eliminar(
+@Id_Contact int
+)
+as
+begin
+	delete from CONTACTO where IdContact = @Id_Contact
+end
