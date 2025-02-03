ed124d6a5a95d44f5faf5f1877db8a251b71edc4
Abel Martinez <abelrobles0409@gmail.com>
2025-02-03 10:23:28 -0400

    Creacion de clase

---
+++ Tarea2/CRUDCORE/Models/ContactoModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CRUDCORE.Models
+{
+    public class ContactoModel
+    {
+
+        public int IdContact { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Telefono { get; set; }
+
+        public string? Correo { get; set; }
+    }
+}
