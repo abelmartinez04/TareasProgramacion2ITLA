18b4a7f34c4de27e67be8587d6061b4a6d6f1407
Abel Martinez <abelrobles0409@gmail.com>
2025-02-04 11:38:16 -0400

    Algunas restricciones para los campos del formulario

--- Tarea2/CRUDCORE/Models/ContactoModel.cs
+++ Tarea2/CRUDCORE/Models/ContactoModel.cs
@@ -1,14 +1,18 @@
-﻿namespace CRUDCORE.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDCORE.Models
 {
     public class ContactoModel
     {
 
         public int IdContact { get; set; }
 
+        [Required]
         public string? Nombre { get; set; }
-
+        [Required]
         public string? Telefono { get; set; }
-
+        [Required]
         public string? Correo { get; set; }
     }
 }
+// me quede aqui https://youtu.be/qcRCDM5KiSo?si=jh6tlTGTt3mNkyJw
