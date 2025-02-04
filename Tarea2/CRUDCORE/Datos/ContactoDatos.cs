0303695ea34648224b4d8189c44bfed5aea5bbfc
Abel Martinez <abelrobles0409@gmail.com>
2025-02-04 10:04:38 -0400

    Terminamos de definir los constructores de los procedimientos de la conexion con la base de datos

--- Tarea2/CRUDCORE/Datos/ContactoDatos.cs
+++ Tarea2/CRUDCORE/Datos/ContactoDatos.cs
@@ -68,7 +68,7 @@ namespace CRUDCORE.Datos
             }
             return oContacto;
         }
-    
+
         public bool Guardar(ContactoModel ocontacto)
         {
             bool rpta;
@@ -91,13 +91,73 @@ namespace CRUDCORE.Datos
             }
             catch (Exception e)
             {
-                string errro = e.Message;
-                rpta=false;
+                string error = e.Message;
+                rpta = false;
             }
 
 
             return rpta;
-            //me quede aqui: https://youtu.be/qcRCDM5KiSo?si=Nv8yuAN3M6cfiSXW&t=2321
+
+        }
+
+        public bool Editar(ContactoModel ocontacto)
+        {
+            bool rpta;
+
+            try
+            {
+                var cn = new Conexion();
+
+                using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Editar", conexion);
+                    cmd.Parameters.AddWithValue("IdContact", ocontacto.IdContact);
+                    cmd.Parameters.AddWithValue("Nombre", ocontacto.Nombre);
+                    cmd.Parameters.AddWithValue("Telefono", ocontacto.Telefono);
+                    cmd.Parameters.AddWithValue("Correo", ocontacto.Correo);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                rpta = false;
+            }
+
+
+            return rpta;
+        }
+
+
+        public bool Eliminar(int IdContact)
+        {
+            bool rpta;
+
+            try
+            {
+                var cn = new Conexion();
+
+                using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Eliminar", conexion);
+                    cmd.Parameters.AddWithValue("IdContact", IdContact);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                rpta = false;
+            }
+
+
+            return rpta;
         }
 
     }
